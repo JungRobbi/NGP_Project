bbc6ae8e9287aabd72edc9c528a30b5c5e1f7898
Park Jea Woo <jjjj901913@gmail.com>
2022-11-16 11:58:05 +0900

    PlayerLeave함수 내용 추가

--- Server/PlayerLeave.h
+++ Server/PlayerLeave.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Common.h"
+#include "GameData.h"
+
+void sendPlayerLeave(SOCKET sock, Leave data)
+{
+	int retval;
+	retval = send(sock, (char*)&data, sizeof(data), 0);
+	if (retval == SOCKET_ERROR) {
+		err_display("send()");
+		return;
+	}
+
+}
+
+void recvPlayerLeave(SOCKET sock)
+{
+	int retval;
+	PlayerInfoScene data;
+	retval = recv(sock, (char*)&data, sizeof(data), 0);
+}
