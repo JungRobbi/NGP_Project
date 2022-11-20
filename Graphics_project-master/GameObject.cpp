e973dc7b0fca5d02bf44b2cbe20d2b2e9b555ac0
이서연 <75468561+truebird2201@users.noreply.github.com>
2022-11-20 16:14:08 +0900

    Lobby화면 생성 ( n_scene = 10 )

--- Graphics_project-master/GameObject.cpp
+++ Graphics_project-master/GameObject.cpp
@@ -15,17 +15,8 @@ void GameObject::render()
 		return;
 	auto tr = GetComponent<Transform3D>()->mat_Transform;
 
-	if (obj_num == 1) {
-		glUniform4f(Pcolor, 1.0, 0.0, 0.0, 0.0);
-	}
-	else {
-		glUniform4f(Pcolor, 0.0, 0.0, 0.0, 0.0);
-	}
-
 	glBindVertexArray(VAO);
 	glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(tr));
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glDrawArrays(GL_TRIANGLES, 0, num_index);
-
-
 }
